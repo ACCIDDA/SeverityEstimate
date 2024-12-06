310bed7975e0ee6cd289f6f743555142cdf48690
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2024-12-06 13:34:17 -0500

    Add checkmate ext for probabilities
    Added `check_probability`, `assert_probability` utilities for validating
    probability type arguments.

---
+++ R/checkmate_extensions.R
@@ -0,0 +1,20 @@
+#' @title
+#' Checkmate Extensions For Probabilities
+#'
+#' @description
+#' This is a small wrapper around [checkmate::check_number()] specifically for
+#' probabilities for convenience.
+#'
+#' @inheritParams checkmate::check_number
+#'
+#' @importFrom checkmate check_number
+#' @importFrom checkmate makeAssertionFunction
+#' @keywords internal
+check_probability <- function(x) {
+  check_number(
+    x, na.ok = FALSE, lower = 0.0, upper = 1.0, finite = TRUE, null.ok = TRUE
+  )
+}
+
+#' @rdname check_probability
+assert_probability <- checkmate::makeAssertionFunction(check_probability)
