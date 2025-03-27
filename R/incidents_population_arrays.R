f8dd7ed5f5ef68ae8a61ae56fcdb15831b370174
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-03-27 08:51:41 -0400

    Add `incidents_population_arrays`
    Ported over utility to convert a linelist into incidents and population
    arrays with appropriate time, strata, surveillance, and outcome
    dimensions.

---
+++ R/incidents_population_arrays.R
@@ -0,0 +1,189 @@
+#' @title Convert Incidents And Populations To Like Shaped Arrays
+#'
+#' @description
+#' Create an incidents and population arrays that are compatible in shape from
+#' a data.frame like line list and population.
+#'
+#' @param linelist A line list to convert into an array.
+#' @param population Population data set to convert into an array.
+#' @param time_period A character of columns describing the time period such as
+#' 'week' or 'day'
+#' @param strata A character of columns describing the attributes to stratify
+#' the data on.
+#' @param surveillance A character of columns describing the surveillance
+#' methods.
+#' @param outcome A character of columns describing the outcome of the line list
+#' entry.
+#' @param population_value A unit length character corresponding to the column
+#' in the `population` data.frame (or data.frame like object) for the population
+#' value.
+#'
+#' @details
+#' It is expected that all of the `time_period`, `strata`, `surveillance`, and
+#' `outcome` columns are all present in `linelist`. All of the `strata` columns
+#' must be in `population`.
+#'
+#' @return
+#' A list with names 'incidents', 'population', 'time_period', 'strata',
+#' 'surveillance', and 'outcome'. These named list elements correspond to:
+#' * `incidents`: A four dimensional array with the dimensions referring to
+#' `time_period`, `strata`, `surveillance`, and `outcome`.
+#' * `population`: A one dimensional array with the dimensions referring to
+#' `strata`.
+#' * `time_period`: A data.frame constructed from the `time_period` columns
+#' given and whose rownames correspond to the `time_period` dimension.
+#' * `strata`: A data.frame constructed from the `strata` columns given and
+#' whose rownames correspond to the `strata` dimension.
+#' * `surveillance`: A data.frame constructed from the `surveillance` columns
+#' given and whose rownames correspond to the `surveillance` dimension.
+#' * `outcome`: A data.frame constructed from the `outcome` columns given and
+#' whose rownames correspond to the `outcome` dimension.
+#' * `linelist_ind`: An integer matrix with the same number of rows as
+#' `linelist` and columns corresponding to the dimensions extracted. The values
+#' indicate where each line list element corresponds to in each of those
+#' dimensions.
+#'
+#' The strata dimensions of `incidents` and `population` will be the same.
+#'
+#' @importFrom utils tail
+#' @keywords internal
+incidents_population_arrays <- function(
+  linelist,
+  population,
+  time_period,
+  strata,
+  surveillance,
+  outcome,
+  population_value,
+  time_period_reference,
+  strata_reference,
+  surveillance_reference,
+  outcome_reference
+) {
+  # Downcast to data.frame for simplicity
+  if (!setequal(class(linelist), "data.frame")) {
+    linelist <- as.data.frame(linelist)
+  }
+  if (!setequal(class(population), "data.frame")) {
+    population <- as.data.frame(population)
+  }
+
+  # Input validation
+  stopifnot(
+    all(time_period %in% names(linelist)),
+    all(strata %in% names(linelist)),
+    all(surveillance %in% names(linelist)),
+    all(outcome %in% names(linelist)),
+    all(strata %in% names(population)),
+    length(population_value) == 1L,
+    population_value %in% names(population)
+  )
+  time_period_reference <- process_reference(time_period_reference, time_period)
+  strata_reference <- process_reference(strata_reference, strata)
+  surveillance_reference <- process_reference(
+    surveillance_reference, surveillance
+  )
+  outcome_reference <- process_reference(outcome_reference, outcome)
+
+  # Time period/surveillance/outcome are only in `linelist` so start there
+  extract_list <- extract_and_match_data_frame(
+    linelist,
+    time_period,
+    subset_x = time_period_reference
+  )
+  extract_time_period <- extract_list$df
+  linelist_time_period_ind <- extract_list$ind
+
+  extract_list <- extract_and_match_data_frame(
+    linelist,
+    surveillance,
+    subset_x = surveillance_reference
+  )
+  extract_surveillance <- extract_list$df
+  linelist_surveillance_ind <- extract_list$ind
+
+  extract_list <- extract_and_match_data_frame(
+    linelist,
+    outcome,
+    subset_x = outcome_reference
+  )
+  extract_outcome <- extract_list$df
+  linelist_outcome_ind <- extract_list$ind
+
+  # Extract strata
+  extract_df <- strata_reference
+  if (is.null(strata_reference)) {
+    extract_df <- unique(
+      rbind(
+        unique(linelist[, strata, drop = FALSE]),
+        unique(population[, strata, drop = FALSE])
+      )
+    )
+  }
+  extract_list <- extract_and_match_data_frame(
+    population,
+    strata,
+    subset_x = extract_df
+  )
+  extract_strata <- extract_list$df
+  population_strata_ind <- extract_list$ind
+
+  linelist_strata_ind <- match(
+    interaction(linelist[, strata]),
+    interaction(extract_strata)
+  )
+
+  linelist_extra_strata_ind <- setdiff(
+    linelist_strata_ind, population_strata_ind
+  )
+  if (length(linelist_extra_strata_ind) > 0L) {
+    stop(
+      "There are `strata` values found in the line ",
+      "list that are not found in the population."
+    )
+  }
+
+  # Build the `incidents` and `population` arrays
+  population <- construct_array_from_indices(
+    population_strata_ind,
+    target = population[, population_value],
+    dim = nrow(extract_strata),
+    dimnames = "strata"
+  )
+  incidents <- construct_array_from_indices(
+    linelist_time_period_ind,
+    linelist_strata_ind,
+    linelist_surveillance_ind,
+    linelist_outcome_ind,
+    dim = c(
+      nrow(extract_time_period),
+      nrow(extract_strata),
+      nrow(extract_surveillance),
+      nrow(extract_outcome)
+    ),
+    dimnames = c("time_period", "strata", "surveillance", "outcome")
+  )
+
+  # Build the indices matrix
+  linelist_ind <- cbind(
+    linelist_time_period_ind,
+    linelist_strata_ind,
+    linelist_surveillance_ind,
+    linelist_outcome_ind
+  )
+  dimnames(linelist_ind) <- list(
+    "row" = seq_along(linelist_time_period_ind),
+    "dimension" = c("time_period", "strata", "surveillance", "outcome")
+  )
+
+  # Done
+  list(
+    "incidents" = incidents,
+    "population" = population,
+    "time_period" = extract_time_period,
+    "strata" = extract_strata,
+    "surveillance" = extract_surveillance,
+    "outcome" = extract_outcome,
+    "linelist_ind" = linelist_ind
+  )
+}
---
+++ tests/testthat/test-incidents_population_arrays.R
@@ -0,0 +1,442 @@
+test_that("Input Validation", {
+  # Test values to work with
+  linelist <- data.frame(
+    patient_id = letters[1L:6L],
+    week = c(1L, 1L, 1L, 2L, 2L, 3L),
+    sex = c("F", "F", "M", "M", "M", "F"),
+    testing = c("Active", "Passive", "Active", "Active", "Active", "Active"),
+    patient_status = c("A", "A", "S", "S", "D", "S")
+  )
+  population <- data.frame(
+    sex = c("F", "M"),
+    total = c(2000L, 1950L)
+  )
+
+  # Basic tests
+  expect_error(
+    incidents_population_arrays(
+      linelist,
+      population,
+      "date",
+      "sex",
+      "testing",
+      "patient_status",
+      "total",
+      NULL,
+      NULL,
+      NULL,
+      NULL
+    ),
+    "all(time_period %in% names(linelist)) is not TRUE",
+    fixed = TRUE
+  )
+  expect_error(
+    incidents_population_arrays(
+      linelist,
+      population,
+      "week",
+      "age",
+      "testing",
+      "patient_status",
+      "total",
+      NULL,
+      NULL,
+      NULL,
+      NULL
+    ),
+    "all(strata %in% names(linelist)) is not TRUE",
+    fixed = TRUE
+  )
+  expect_error( # Specifically gets `population`
+    incidents_population_arrays(
+      linelist,
+      population,
+      "week",
+      "week",
+      "testing",
+      "patient_status",
+      "total",
+      NULL,
+      NULL,
+      NULL,
+      NULL
+    ),
+    "all(strata %in% names(population)) is not TRUE",
+    fixed = TRUE
+  )
+  expect_error(
+    incidents_population_arrays(
+      linelist,
+      population,
+      "week",
+      "sex",
+      "test_type",
+      "patient_status",
+      "total",
+      NULL,
+      NULL,
+      NULL,
+      NULL
+    ),
+    "all(surveillance %in% names(linelist)) is not TRUE",
+    fixed = TRUE
+  )
+  expect_error(
+    incidents_population_arrays(
+      linelist,
+      population,
+      "week",
+      "sex",
+      "testing",
+      "individual_outcome",
+      "total",
+      NULL,
+      NULL,
+      NULL,
+      NULL
+    ),
+    "all(outcome %in% names(linelist)) is not TRUE",
+    fixed = TRUE
+  )
+  expect_error(
+    incidents_population_arrays(
+      linelist,
+      population,
+      "week",
+      "sex",
+      "testing",
+      "patient_status",
+      c("total_a", "total_b"),
+      NULL,
+      NULL,
+      NULL,
+      NULL
+    ),
+    "length(population_value) == 1L is not TRUE",
+    fixed = TRUE
+  )
+  expect_error(
+    incidents_population_arrays(
+      linelist,
+      population,
+      "week",
+      "sex",
+      "testing",
+      "patient_status",
+      "strata_size",
+      NULL,
+      NULL,
+      NULL,
+      NULL
+    ),
+    "population_value %in% names(population) is not TRUE",
+    fixed = TRUE
+  )
+})
+
+test_that("Incidents Strata Variables Not Covered By Population", {
+  linelist <- data.frame(
+    patient_id = letters[1L:7L],
+    week = c(1L, 1L, 1L, 2L, 2L, 3L, 3L),
+    sex = c("F", "F", "M", "M", "M", "F", "NA"),
+    testing = c(
+      "Active", "Passive", "Active", "Active", "Active", "Active", "Passive"
+    ),
+    status = c("A", "S", "S", "S", "D", "D", "S")
+  )
+  population <- data.frame(
+    sex = c("F", "M"),
+    total = c(2000L, 1950L)
+  )
+  expect_error(
+    incidents_population_arrays(
+      linelist,
+      population,
+      "week",
+      "sex",
+      "testing",
+      "status",
+      "total",
+      NULL,
+      NULL,
+      NULL,
+      NULL
+    ),
+    paste0(
+      "There are `strata` values found in the line ",
+      "list that are not found in the population."
+    ),
+    fixed = TRUE
+  )
+})
+
+test_that("Output Validation", {
+  # Basic test
+  linelist <- data.frame(
+    patient_id = letters[1L:6L],
+    week = c(1L, 1L, 1L, 2L, 2L, 3L),
+    sex = c("F", "F", "M", "M", "M", "F"),
+    testing = c("Active", "Passive", "Active", "Active", "Active", "Active"),
+    patient_outcome = c(
+      "Asymptomatic", "Asymptomatic", "Symptomatic",
+      "Death",        "Symptomatic",  "Symptomatic"
+    )
+  )
+  population_without_time <- data.frame(
+    sex = c("F", "M"),
+    total = c(2000L, 1950L)
+  )
+  output_arrays <- incidents_population_arrays(
+    linelist,
+    population_without_time,
+    "week",
+    "sex",
+    "testing",
+    "patient_outcome",
+    "total",
+    NULL,
+    NULL,
+    NULL,
+    NULL
+  )
+  expect_equal(
+    names(output_arrays),
+    c(
+      "incidents", "population", "time_period", "strata",
+      "surveillance", "outcome", "linelist_ind"
+    )
+  )
+  expected_incidents <- array(
+    data = c(
+      1L, 0L, 0L,
+      0L, 0L, 0L,
+      1L, 0L, 0L,
+      0L, 0L, 0L,
+      0L, 0L, 0L,
+      0L, 1L, 0L,
+      0L, 0L, 0L,
+      0L, 0L, 0L,
+      0L, 0L, 1L,
+      1L, 1L, 0L,
+      0L, 0L, 0L,
+      0L, 0L, 0L
+    ),
+    dim = c(3L, 2L, 2L, 3L),
+    dimnames = list(
+      "time_period" = c("1", "2", "3"),
+      "strata" = c("1", "2"),
+      "surveillance" = c("1", "2"),
+      "outcome" = c("1", "2", "3")
+    )
+  )
+  expect_identical(output_arrays$incidents, expected_incidents)
+  expected_population <- array(
+    data = c(2000L, 1950L),
+    dimnames = list("strata" = c("1", "2"))
+  )
+  expect_identical(output_arrays$population, expected_population)
+  expected_time_period <- data.frame(week = 1L:3L)
+  expect_identical(output_arrays$time_period, expected_time_period)
+  expected_strata <- data.frame(sex = c("F", "M"))
+  expect_identical(output_arrays$strata, expected_strata)
+  expected_surveillance <- data.frame(testing = c("Active", "Passive"))
+  expect_identical(output_arrays$surveillance, expected_surveillance)
+  expect_identical(
+    output_arrays$outcome,
+    data.frame(patient_outcome = c("Asymptomatic", "Death", "Symptomatic"))
+  )
+
+  # Larger test
+  linelist <- data.frame(
+    patient_id = letters[1L:12L],
+    month = rep(c(1L, 2L), each = 6L),
+    day = c(1L, 1L, 2L, 3L, 3L, 3L, 1L, 1L, 4L, 4L, 4L, 4L),
+    sex = c("F", "F", "M", "M", "M", "F", "M", "M", "F", "F", "F", "M"),
+    age_category = c(
+      "0-17", "0-17", "0-17", "18+", "18+", "18+",
+      "0-17", "0-17", "0-17", "18+", "18+", "18+"
+    ),
+    testing = rep_len(c("Active", "Passive"), 12L),
+    status = c(rep("Recovered", 10L), rep("Death", 2L))
+  )
+  population <- data.frame(
+    sex = c("F", "M", "F", "M"),
+    age_category = c("0-17", "0-17", "18+", "18+"),
+    total = c(2000L, 1950L, 8000L, 7800L)
+  )
+  output_arrays <- incidents_population_arrays(
+    linelist,
+    population,
+    c("month", "day"),
+    c("sex", "age_category"),
+    "testing",
+    "status",
+    "total",
+    NULL,
+    NULL,
+    NULL,
+    NULL
+  )
+  expect_equal(
+    names(output_arrays),
+    c(
+      "incidents", "population", "time_period", "strata",
+      "surveillance", "outcome", "linelist_ind"
+    )
+  )
+  expected_incidents <- array(
+    data = c(
+      # 1L, 0L, 0L, 0L, 1L,
+      # 0L, 0L, 0L, 0L, 1L,
+      # 0L, 1L, 0L, 1L, 0L,
+      # 0L, 0L, 1L, 0L, 0L,
+      # 1L, 0L, 0L, 0L, 0L,
+      # 0L, 0L, 1L, 0L, 1L,
+      # 0L, 0L, 0L, 1L, 0L,
+      # 0L, 0L, 1L, 0L, 1L
+
+      0L, 0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L, 1L,
+      0L, 0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L, 1L,
+      1L, 0L, 0L, 0L, 1L,
+      0L, 0L, 0L, 0L, 0L,
+      0L, 1L, 0L, 1L, 0L,
+      0L, 0L, 1L, 0L, 0L,
+      1L, 0L, 0L, 0L, 0L,
+      0L, 0L, 1L, 0L, 1L,
+      0L, 0L, 0L, 1L, 0L,
+      0L, 0L, 1L, 0L, 0L
+    ),
+    dim = c(5L, 4L, 2L, 2L),
+    dimnames = list(
+      "time_period" = c("1", "2", "3", "4", "5"),
+      "strata" = c("1", "2", "3", "4"),
+      "surveillance" = c("1", "2"),
+      "outcome" = c("1", "2")
+    )
+  )
+  expect_identical(output_arrays$incidents, expected_incidents)
+  expected_population <- array(
+    data = c(2000L, 8000L, 1950L, 7800L),
+    dimnames = list("strata" = c("1", "2", "3", "4"))
+  )
+  expect_identical(output_arrays$population, expected_population)
+  expected_time_period <- data.frame(
+    month = c(1L, 1L, 1L, 2L, 2L),
+    day = c(1L, 2L, 3L, 1L, 4L)
+  )
+  expect_identical(output_arrays$time_period, expected_time_period)
+  expected_strata <- data.frame(
+    sex = c("F", "F", "M", "M"),
+    age_category = c("0-17", "18+", "0-17", "18+")
+  )
+  expected_surveillance <- data.frame(testing = c("Active", "Passive"))
+  expect_identical(output_arrays$surveillance, expected_surveillance)
+  expect_identical(
+    output_arrays$outcome,
+    data.frame(status = c("Death", "Recovered"))
+  )
+  expected_linelist_ind <- matrix(
+    data = c(
+      1L, 1L, 2L, 3L, 3L, 3L, 4L, 4L, 5L, 5L, 5L, 5L,
+      1L, 1L, 3L, 4L, 4L, 2L, 3L, 3L, 1L, 2L, 2L, 4L,
+      1L, 2L, 1L, 2L, 1L, 2L, 1L, 2L, 1L, 2L, 1L, 2L,
+      2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 1L, 1L
+    ),
+    nrow = 12L,
+    ncol = 4L,
+    dimnames = list(
+      "row" = seq_len(12L),
+      "dimension" = c("time_period", "strata", "surveillance", "outcome")
+    )
+  )
+  expect_identical(output_arrays$linelist_ind, expected_linelist_ind)
+})
+
+test_that("Output Validation When Given Reference data.frames", {
+  linelist <- data.frame(
+    patient_id = letters[1L:6L],
+    week = c(1L, 1L, 1L, 2L, 2L, 3L),
+    sex = c("F", "F", "M", "M", "M", "F"),
+    testing = c("Active", "Passive", "Active", "Active", "Active", "Active"),
+    patient_outcome = c(
+      "Asymptomatic", "Asymptomatic", "Symptomatic",
+      "Death",        "Symptomatic",  "Symptomatic"
+    )
+  )
+  population_without_time <- data.frame(
+    sex = c("F", "M"),
+    total = c(2000L, 1950L)
+  )
+  time_period_reference <- data.frame(week = 1L:4L)
+  strata_reference <- data.frame(sex = c("F", "M", "NA"))
+  output_arrays <- incidents_population_arrays(
+    linelist,
+    population_without_time,
+    "week",
+    "sex",
+    "testing",
+    "patient_outcome",
+    "total",
+    time_period_reference,
+    strata_reference,
+    NULL,
+    NULL
+  )
+
+  expect_equal(
+    names(output_arrays),
+    c(
+      "incidents", "population", "time_period", "strata",
+      "surveillance", "outcome", "linelist_ind"
+    )
+  )
+  expected_incidents <- array(
+    data = c(
+      1L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L,
+      1L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L,
+      0L, 1L, 0L, 0L,
+      0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L,
+      0L, 0L, 1L, 0L,
+      1L, 1L, 0L, 0L,
+      0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L,
+      0L, 0L, 0L, 0L
+    ),
+    dim = c(4L, 3L, 2L, 3L),
+    dimnames = list(
+      "time_period" = c("1", "2", "3", "4"),
+      "strata" = c("1", "2", "3"),
+      "surveillance" = c("1", "2"),
+      "outcome" = c("1", "2", "3")
+    )
+  )
+  expect_identical(output_arrays$incidents, expected_incidents)
+  expected_population <- array(
+    data = c(2000L, 1950L, 0L),
+    dimnames = list("strata" = c("1", "2", "3"))
+  )
+  expect_identical(output_arrays$population, expected_population)
+  expect_identical(output_arrays$time_period, time_period_reference)
+  expect_identical(output_arrays$strata, strata_reference)
+  expected_surveillance <- data.frame(testing = c("Active", "Passive"))
+  expect_identical(output_arrays$surveillance, expected_surveillance)
+  expect_identical(
+    output_arrays$outcome,
+    data.frame(patient_outcome = c("Asymptomatic", "Death", "Symptomatic"))
+  )
+})
