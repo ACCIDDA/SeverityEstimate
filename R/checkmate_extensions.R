a34ff3f936470531150ee506b77010bc928c2c2f
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-07-15 13:51:26 -0400

    Generalize checkmate extensions
    Generalized checkmate extensions from being probability specific to
    other types of checks, in this case `assert_bool`. The changes involved
    are:
    
    * Documentation adjustments to keep checkmate extensions documented
      together.
    * Added `assert_bool` internal helper.
    * Added `na.ok` and `null.ok` arguments to
      `check_probability`/`assert_probability`.

--- R/checkmate_extensions.R
+++ R/checkmate_extensions.R
@@ -1,25 +1,45 @@
 #' @title
-#' Checkmate Extensions For Probabilities
+#' Checkmate Extensions
 #'
 #' @description
-#' This is a small wrapper around [checkmate::check_number()] specifically for
-#' probabilities for convenience.
+#' Convenience wrappers around checkmate for specific internal use cases.
+#' Namely:
+#' * `check_probability`/`assert_probability` are wrappers around
+#' [checkmate::check_number()] specifically for probabilities.
+#' * `assert_bool` is a wrappeer around [checkmate::check_false()] and
+#' [checkmate::check_true()] for single length logicals.
 #'
 #' @inheritParams checkmate::check_number
+#' @inheritParams checkmate::check_false
+#' @inheritParams checkmate::check_true
 #'
+#' @importFrom checkmate assert
+#' @importFrom checkmate check_false
 #' @importFrom checkmate check_number
+#' @importFrom checkmate check_true
 #' @importFrom checkmate makeAssertionFunction
 #' @keywords internal
-check_probability <- function(x) {
+#' @rdname checkmate_extensions
+check_probability <- function(x, na.ok = FALSE, null.ok = FALSE) {
   checkmate::check_number(
     x,
-    na.ok = FALSE,
+    na.ok = na.ok,
     lower = 0.0,
     upper = 1.0,
     finite = TRUE,
-    null.ok = TRUE
+    null.ok = null.ok
   )
 }
 
-#' @rdname check_probability
+#' @rdname checkmate_extensions
 assert_probability <- checkmate::makeAssertionFunction(check_probability)
+
+#' @rdname checkmate_extensions
+assert_bool <- function(x, na.ok = FALSE, .var.name = checkmate::vname(x)) {
+  checkmate::assert(
+    checkmate::check_true(x, na.ok = na.ok),
+    checkmate::check_false(x, na.ok = na.ok),
+    combine = "or",
+    .var.name = .var.name
+  )
+}
