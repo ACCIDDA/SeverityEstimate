cad8156a893d65be6ab840f12d76af93d7710b04
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-07-15 13:51:26 -0400

    Generalize checkmate extensions
    Generalized checkmate extensions from being probability specific to
    other types of checks, in this case `assert_bool`.

--- R/checkmate_extensions.R
+++ R/checkmate_extensions.R
@@ -1,15 +1,28 @@
 #' @title
-#' Checkmate Extensions For Probabilities
+#' Checkmate Extensions
 #'
 #' @description
 #' This is a small wrapper around [checkmate::check_number()] specifically for
 #' probabilities for convenience.
 #'
-#' @inheritParams checkmate::check_number
+#' Convenience wrappers around checkmate for specific internal use cases.
+#' Namely:
+#' * `check_probability`/`assert_probability` are wrappers around
+#' [checkmate::check_number()] specifically for probabilities, and
+#' * `assert_bool` is a wrappeer around [checkmate::check_false()] and
+#' [checkmate::check_true()] for single length logicals.
 #'
+#' @inheritParams checkmate::check_number
+#' @inheritParams checkmate::check_false
+#' @inheritParams chekcamte::check_true
+#'
+#' @importFrom checkmate assert
+#' @importFrom checkmate check_false
 #' @importFrom checkmate check_number
+#' @importFrom checkmate check_true
 #' @importFrom checkmate makeAssertionFunction
 #' @keywords internal
+#' @rdname checkmate_extensions
 check_probability <- function(x) {
   checkmate::check_number(
     x,
@@ -21,5 +34,15 @@ check_probability <- function(x) {
   )
 }
 
-#' @rdname check_probability
+#' @rdname checkmate_extensions
 assert_probability <- checkmate::makeAssertionFunction(check_probability)
+
+#' @rdname checkmate_extensions
+assert_bool <- function(x, na.ok = FALSE, .var.name = checkmate::vname(x)) {
+  checkmate::assert(
+    checkmate::check_true(x, na.ok = na.ok),
+    checkmate::check_false(x, na.ok = na.ok),
+    combine = "or",
+    .var.name = .var.name
+  )
+}
