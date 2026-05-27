e56a9def70971aa2d1cf28a5506e758c8d6d19f4
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2026-05-27 13:28:39 -0400

    Remove `estimate_severity` API
    Removed the `SeverityEstimate::estimate_severity` function, which has
    been superseded by the tidymodels style API.
    
    See https://github.com/ACCIDDA/SeverityEstimate/milestone/1.
    
    Closes #88.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -13,11 +13,9 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 
 
 RcppExport SEXP _rcpp_module_boot_stan_fit4estimate_severity_mod();
-RcppExport SEXP _rcpp_module_boot_stan_fit4severity_estimate_by_surveillance_mod();
 
 static const R_CallMethodDef CallEntries[] = {
     {"_rcpp_module_boot_stan_fit4estimate_severity_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4estimate_severity_mod, 0},
-    {"_rcpp_module_boot_stan_fit4severity_estimate_by_surveillance_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4severity_estimate_by_surveillance_mod, 0},
     {NULL, NULL, 0}
 };
 
--- tests/testthat/test-stan_model.R
+++ tests/testthat/test-stan_model.R
@@ -1,16 +1,3 @@
-test_that("By-surveillance Stan model compiles", {
-  output <- rstan::stanc(
-    file = system.file(
-      "stan",
-      "severity_estimate_by_surveillance.stan",
-      package = "SeverityEstimate"
-    ),
-    model_name = "severity_estimate_by_surveillance",
-    verbose = FALSE
-  )
-  expect_true(output$status)
-})
-
 test_that("`stan_model()` errors for an unknown precompiled model", {
   expect_error(
     stan_model("does_not_exist"),
